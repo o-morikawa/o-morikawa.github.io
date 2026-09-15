44980a153e8fae4736791b643545adab90cfd8c7
Okuto Morikawa <107906773+o-morikawa@users.noreply.github.com>
2026-09-15 15:39:14 +0900

    Delete database directory

--- database/build.sh
+++
@@ -1,4 +0,0 @@
-python3 scripts/import_bib.py sources/ref_om.bib -o data/publications.yaml
-python3 scripts/build.py
-python3 scripts/merge_researchmap.py sources/rm_researchers.jsonl
-python3 scripts/build.py
